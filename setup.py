a8d0bf21307ae423f6fb0a6cd0598371737b0b76
atsuo ishimoto <atsuoishimoto@gmail.com>
2017-06-02 00:12:59 +0900

    Update MANIFEST.in

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def list_packages(root):
 
 setup(
     name="miyadaiku.themes.jquery",
-    version="0.0.2",
+    version="0.0.3",
     author="Atsuo Ishimoto",
     license="MIT",
     classifiers=[
