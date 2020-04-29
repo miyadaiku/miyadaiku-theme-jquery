50a34c10ebc26ce1829e1d5f1de2599c50572c74
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-29 17:00:21 +0900

    update files

--- setup.py
+++ setup.py
@@ -1,48 +1,15 @@
-import os
-import re
 import pathlib
-import distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 from miyadaiku import setuputils
 
 DIR = pathlib.Path(__file__).resolve().parent
-
-requires = [
-    "miyadaiku"
-]
 
 srcdir = DIR / 'node_modules/jquery/dist/'
 destdir = DIR / 'miyadaiku_theme_jquery/externals/'
 copy_files = [[srcdir, ['jquery*.js'], destdir]]
 
-versionpy = DIR / 'miyadaiku_theme_jquery/__version__.py'
-version = re.search(r'"([\d.]+)"', versionpy.read_text())[1]
-
 
 setup(
-    name="miyadaiku_theme_jquery",
-    version=version,
-    author="Atsuo Ishimoto",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-    ],
-    description='jQuery files for miyadaiku static site generator',
-    long_description=setuputils.read_file(DIR, 'README.rst'),
-    url='https://github.com/miyadaiku/miyadaiku-theme-jquery',
-    project_urls={
-        'Miyadaiku': 'https://miyadaiku.github.io/',
-    },
-
-    packages=list(setuputils.list_packages(DIR, 'miyadaiku_theme_jquery')),
-    package_data={
-        '': setuputils.SETUP_FILE_EXTS,
-    },
-    install_requires=requires,
-    include_package_data=True,
-    zip_safe=False,
     cmdclass={'copy_files': setuputils.copy_files},
     copy_files=copy_files
 )
